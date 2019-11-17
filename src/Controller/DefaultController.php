0e1572f3a7d625255ec3c5dd02c63328f25d5ef4
AndresDav7 <andres.alvear01@epn.edu.ec>
2019-11-17 10:38:38 -0500

    RelacionPaisCiudad

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 
-use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,7 +14,7 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="homepage", methods={"GET"})
      */
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(): Response
     {
         return $this->render('default/index.html.twig');
     }
